29c94873a34684bd1f157f056c6600e30748ec0c
Mika Brk <mikab@Mikas-MacBook-Pro.local>
2026-05-29 13:26:45 +0200

    nadodao sam ikone u dugma na start pageu ali nisam uspio preko import i patha pa sam trebao pre SVG

--- vue-GymFlow/src/views/HomeView.vue
+++ vue-GymFlow/src/views/HomeView.vue
@@ -22,26 +22,77 @@
 
    <div class="menu-grid">
     <button class="menu-card" @click="navigate('profil')">
+        <div class="@menu-icon">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
+        </div>
         <h3> Profil </h3>
         <p>Upravljajte svojim profilom i tjelesnim podacima</p>
     </button>
+
+
     <button class="menu-card" @click="navigate('ciljevi')">
+        <div class="menu-icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  </div>
         <h3> Ciljevi </h3>
         <p>Definirajte fitness ciljeve</p>
     </button>
+
+
     <button class="menu-card" @click="navigate('plan-treninga')">
+        <div class="menu-icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+    </svg>
+  </div>
         <h3> Plan treninga </h3>
         <p>Tjedni plan i predlošci</p>
     </button>
+
+
     <button class="menu-card" @click="navigate('logiraj-trening')">
+        <div class="menu-icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M12 4v16m8-8H4" />
+    </svg>
+  </div>
         <h3> Logiraj trening </h3>
         <p>Unesite današnji trening</p>
     </button>
+
+
     <button class="menu-card" @click="navigate('napredak')">
+        <div class="menu-icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 
+        0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 
+        2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+    </svg>
+  </div>
         <h3> Napredak </h3>
         <p>Grafovi i statistika</p>
     </button>
+
+
     <button class="menu-card" @click="navigate('dnevnik')">
+        <div class="menu-icon">
+    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 
+        18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 
+        5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 
+        0-3.332.477-4.5 1.253" />
+    </svg>
+  </div>
         <h3> Dnevnik </h3>
         <p>Povijest treninga</p>
     </button>
